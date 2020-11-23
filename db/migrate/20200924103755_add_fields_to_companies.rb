8bd4eceb9d8f7d9e22c7322ba4afc5ed843e9853
Ahmed Nadar <ahmedfay@hotmail.com>
2020-11-23 16:41:18 +0400

    Update Home companies section. Add fields to companies

--- db/migrate/20200924103755_add_fields_to_companies.rb
+++ db/migrate/20200924103755_add_fields_to_companies.rb
@@ -2,9 +2,8 @@ class AddFieldsToCompanies < ActiveRecord::Migration[6.0]
   def change
     add_column :companies, :headquarter, :string
     add_column :companies, :company_size, :integer
-    add_column :companies, :phone_number, :string
     add_column :companies, :business_model, :string
     add_column :companies, :stage, :string
-    add_column :companies, :industry, :string
+    add_column :companies, :industry, :string    
   end
 end
